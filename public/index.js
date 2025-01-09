21a6f32a7f6bf6d2b5df7262e44e99898793dd7d
SattrFev <airryys@gmail.com>
2025-01-09 08:35:39 +0700

    Haaa

---
+++ public/index.js
@@ -0,0 +1,167 @@
+document.addEventListener("alpine:init", () => {
+  Alpine.data("kategorilow", () => ({
+    kat: [
+      {
+        name: "Development & IT",
+        start: "2.100.000",
+        end: "7.000.000",
+        rek: 17,
+      },
+      {
+        name: "Design & Creative",
+        start: "1.500.000",
+        end: "6.800.000",
+        rek: 8,
+      },
+      {
+        name: "Accountant",
+        start: "1.700.000",
+        end: "6.700.000",
+        rek: 12,
+      },
+      {
+        name: "Social Admin",
+        start: "1.100.000",
+        end: "5.300.000",
+        rek: 9,
+      },
+      {
+        name: "Sales & Marketing",
+        start: "2.000.000",
+        end: "7.500.000",
+        rek: 15,
+      },
+      {
+        name: "Architecture",
+        start: "1.500.000",
+        end: "6.800.000",
+        rek: 7,
+      },
+      {
+        name: "Hospitality",
+        start: "1.700.000",
+        end: "6.700.000",
+        rek: 12,
+      },
+      {
+        name: "Front Office",
+        start: "1.100.000",
+        end: "4.900.000",
+        rek: 7,
+      },
+      {
+        name: "Customer Support",
+        start: "1.200.000",
+        end: "5.200.000",
+        rek: 10,
+      },
+      {
+        name: "Human Resources",
+        start: "1.800.000",
+        end: "6.500.000",
+        rek: 13,
+      },
+      {
+        name: "Education & Training",
+        start: "1.600.000",
+        end: "6.000.000",
+        rek: 9,
+      },
+      {
+        name: "Healthcare",
+        start: "2.000.000",
+        end: "7.200.000",
+        rek: 14,
+      },
+      {
+        name: "Legal",
+        start: "2.100.000",
+        end: "7.500.000",
+        rek: 11,
+      },
+      {
+        name: "Data Analysis",
+        start: "2.200.000",
+        end: "8.000.000",
+        rek: 10,
+      },
+      {
+        name: "Transcription",
+        start: "1.500.000",
+        end: "5.800.000",
+        rek: 8,
+      },
+      {
+        name: "Content Writing",
+        start: "1.700.000",
+        end: "6.200.000",
+        rek: 12,
+      },
+      {
+        name: "Operations",
+        start: "1.800.000",
+        end: "6.900.000",
+        rek: 11,
+      },
+      {
+        name: "Logistics",
+        start: "1.600.000",
+        end: "6.300.000",
+        rek: 9,
+      },
+      {
+        name: "Software Testing",
+        start: "2.000.000",
+        end: "7.500.000",
+        rek: 10,
+      },
+      {
+        name: "E-commerce Support",
+        start: "1.400.000",
+        end: "5.600.000",
+        rek: 8,
+      },
+      {
+        name: "Personal Assistant",
+        start: "1.300.000",
+        end: "5.000.000",
+        rek: 9,
+      },
+      {
+        name: "PR & Communications",
+        start: "1.900.000",
+        end: "6.800.000",
+        rek: 10,
+      },
+      {
+        name: "Video Editing",
+        start: "1.700.000",
+        end: "6.500.000",
+        rek: 11,
+      },
+      {
+        name: "Event Management",
+        start: "1.800.000",
+        end: "6.600.000",
+        rek: 10,
+      },
+    ],
+    blog: [
+      {
+        title:
+          "Membangun Karir Tanpa Batas: Tips untuk Penyandang Disabilitas Memulai di Dunia Kerja",
+        img: "grab.jpg",
+      },
+      {
+        title:
+          "10 Perusahaan yang Berkomitmen pada Inklusi Penyandang Disabilitas di Tempat Kerja",
+        img: "comp.jpg",
+      },
+      {
+        title:
+          "Panduan Menulis CV dan Portofolio yang Menonjol untuk Penyandang Disabilitas",
+        img: "cv.jpg",
+      },
+    ],
+  }));
+});
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}", "node_modules/preline/dist/*.js"],
+  darkMode: false,
+  theme: {
+    extend: {
+      colors: {
+        "ligth-primary": "#48CFCB",
+        "dark-primary": "#229799",
+      },
+      fontFamily: {
+        josefin: ['"Josefin Sans"', "sans-serif"], // Josefin Sans
+        anton: ['"Anton"', "sans-serif"], // Anton
+        poppins: ["Poppins", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+      backgroundPosition: {
+        "center-top": "center 50%", // Horizontal tengah, vertikal 30%
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/forms"), require("preline/plugin")],
+};
